69bb9177e3728ca25d1f4a16117636961ee3c880
David Velasquez <davelasquez12@gmail.com>
2016-02-26 23:09:52 -0600

    Added tabbed layout and viewpager in xml, but still need to wire them up in code

--- app/src/main/java/demo/demoapp/MainActivity.java
+++ app/src/main/java/demo/demoapp/MainActivity.java
@@ -1,15 +1,46 @@
 package demo.demoapp;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends AppCompatActivity
 {
+	private Toolbar mToolbar;
+	private ViewPager mViewPager;
+	private TabLayout mTabLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+		mTabLayout = (TabLayout) findViewById(R.id.main_activity_tab_layout);
+		mViewPager = (ViewPager) findViewById(R.id.main_activity_view_pager);
+
+		setSupportActionBar(mToolbar);
+		setupViewPager(mViewPager);
+		mTabLayout.setupWithViewPager(mViewPager);
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater menuInflater = getMenuInflater();
+		menuInflater.inflate(R.menu.main_menu, menu);
+
+		return true;
+	}
+
+	private void setupViewPager(ViewPager viewPager)
+	{
+
+	}
+
 }
